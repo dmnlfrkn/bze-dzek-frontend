d3239bb9b0e61cf269be8cc6ed951f6963497bf3
Furkan DUMANLI <66632576+dmnlfrkn@users.noreply.github.com>
2025-07-21 16:46:16 +0300

    Updated Footer.jsx

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -59,32 +59,5 @@ function Footer() {
         </footer>
     );
 }
-                    <h3 className="text-green-600 text-base font-bold mb-2">
-                        BZE-DZEK БЗЭЗЭДЗЭКӀ
-                    </h3>
-                    <p>Hasbelkader Çeviri Programı.</p>
-                </div>
-                <div className="text-right">
-                    <h3 className="text-base font-bold mb-2">
-                        Bizi Takip Edin
-                    </h3>
-                    <div className="flex gap-6 justify-end">
-                        <a href="#" className="text-lg hover:text-red-400"><FaLinkedin /></a>
-                        <a href="#" className="text-lg hover:text-red-400"><FaGithub /></a>
-                        <a href="#" className="text-lg hover:text-red-400"><FaInstagram /></a>
-                    </div>
-                </div>
-            </div>
-            <div className="text-center mt-4 border-t border-white/20 pt-2 text-sm">
-                <p>
-                    cCc 2025 <span className="text-green-600">
-                        BZE-DZEK БЗЭЗЭДЗЭКӀ
-                    </span> Tüm hakları satılıktır.
-                </p>
-            </div>
-        </footer>
-    );
-}
 
-export default Footer;
 export default Footer;
